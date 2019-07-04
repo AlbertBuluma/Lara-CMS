a4820c571bcf570ae803f9bf642ce8038ecb24e1
AlbertBuluma <albert.buluma1@gmail.com>
2019-07-04 10:16:05 +0300

    Configure Post table fields.

---
+++ database/migrations/2019_07_03_164007_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('description');
+            $table->text('content');
+            $table->string('Image');
+            $table->timestamp('published_at')->nullable();  //You can create a blog post without filling in this field
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
