c14b364d2561b3e8e0b38a4f3e311d408cc577cb
AlbertBuluma <albert.buluma1@gmail.com>
2019-07-04 15:25:30 +0300

    Add create category request

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateCategoryRequest;
 use App\Category;
 
 class CategoriesController extends Controller
@@ -35,11 +36,11 @@ class CategoriesController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateCategoryRequest $request)
     {
-        $this->validate($request, [
-            'name' => 'required|unique:categories'  //Preventing duplicate categories
-        ]);
+//        $this->validate($request, [
+//            'name' => 'required|unique:categories'  //Preventing duplicate categories
+//        ]);
 
         Category::create([
             'name' => $request->name
