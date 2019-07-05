e9b632b1c73b40c8fd6b449a70e002dbb55c98ec
AlbertBuluma <albert.buluma1@gmail.com>
2019-07-05 16:10:18 +0300

    Add $category->id as parameter

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -20,7 +20,7 @@
                                 {{ $category->name }}
                             </td>
                             <td><a href="{{ route('categories.edit', $category->id) }}" class="btn btn-info btn-sm">Edit</a>
-                            <button class="btn btn-danger btn-sm" onclick="handleDelete()">Delete</button>
+                            <button class="btn btn-danger btn-sm" onclick="handleDelete({{ $category->id }})">Delete</button>
                             </td>
                         </tr>
                     @endforeach
@@ -30,23 +30,39 @@
             <!-- Modal -->
             <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="deleteModalLabel">Modal title</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
+                    <form action="" method="post" id="deleteCategoryForm">  //Action left empty to implement dynamically deleting
+                        @csrf
+                        @method('DELETE')
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="deleteModalLabel">Delete Category</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                ...
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+                                <button type="submit" class="btn btn-danger">Yes, delete</button>
+                            </div>
                         </div>
-                        <div class="modal-body">
-                            ...
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 @endsection
+
+@section('scripts')
+    <script>
+        function handleDelete(id) {
+
+            let form = document.getElementById('deleteCategoryForm');
+            form.action = '/categories/' + id;
+            // console.log('Deleting category', form)
+            $('#deleteModal').modal('show');
+        }
+    </script>
+@endsection    
