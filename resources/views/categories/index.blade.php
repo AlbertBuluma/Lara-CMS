3a5a2190a9810a45a2c475f94745ff36e4267756
AlbertBuluma <albert.buluma1@gmail.com>
2019-07-08 17:37:20 +0300

    Add condition to display categories

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -8,8 +8,9 @@
     <div class="card card-default">
         <div class="card-header">Categories</div>
         <div class="card-body">
-            <table class="table">
-                <thead>
+            @if($categories->count() > 0)
+                <table class="table">
+                    <thead>
                     <th>Name</th>
                     <th></th>
                 </thead>
@@ -20,37 +21,40 @@
                                 {{ $category->name }}
                             </td>
                             <td><a href="{{ route('categories.edit', $category->id) }}" class="btn btn-info btn-sm">Edit</a>
-                            <button class="btn btn-danger btn-sm" onclick="handleDelete({{ $category->id }})">Delete</button>
+                                <button class="btn btn-danger btn-sm" onclick="handleDelete({{ $category->id }})">Delete</button>
                             </td>
                         </tr>
                     @endforeach
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
 
-            <!-- Modal -->
-            <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <form action="" method="post" id="deleteCategoryForm">  //Action left empty to implement dynamically deleting
-                        @csrf
-                        @method('DELETE')
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="deleteModalLabel">Delete Category</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
+                <!-- Modal -->
+                <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <form action="" method="post" id="deleteCategoryForm">  //Action left empty to implement dynamically deleting
+                            @csrf
+                            @method('DELETE')
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="deleteModalLabel">Delete Category</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <p class="text-center text-bold">Are you sure you want to delete this category?</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+                                    <button type="submit" class="btn btn-danger">Yes, delete</button>
+                                </div>
                             </div>
-                            <div class="modal-body">
-                                <p class="text-center text-bold">Are you sure you want to delete this category?</p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
-                                <button type="submit" class="btn btn-danger">Yes, delete</button>
-                            </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
-            </div>
+            @else
+                <h3 class="text-center">No Categories available</h3>
+            @endif
         </div>
     </div>
 @endsection
