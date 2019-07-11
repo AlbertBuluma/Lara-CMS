9b451044b5dfceaf63d8c1ae0e3afd7d0942ac3b
AlbertBuluma <albert.buluma1@gmail.com>
2019-07-11 16:19:42 +0300

    Configure Post relationship

--- app/Category.php
+++ app/Category.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $fillable = ['name']; //Permit mass assignment of name field
+
+    public function posts(){
+
+        return $this->hasMany(Post::class); //A category may have many posts
+    }
 }
