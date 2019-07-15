b02bfbadd1725b8d13e5542b4e86b3b8a8da76a0
AlbertBuluma <albert.buluma1@gmail.com>
2019-07-15 15:58:54 +0300

    Change select element class to form-control

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -37,7 +37,7 @@
             </div>
             <div class="form-group">
                 <label for="category">Category</label>
-                <select name="category" id="category" class="form-group">
+                <select name="category" id="category" class="form-control">
                 @foreach($categories as $category)
                     <option value="{{ $category->id }}">{{ $category->name }}</option>
                 @endforeach
